677b42ec15f68ea5330588f7500b947eefa968a1
0x29A <96635023+SecHex@users.noreply.github.com>
2023-09-16 12:27:37 +0200

    Update Form1.cs

--- SCHXCONFUSION/SCHXCONFUSION/Form1.cs
+++ SCHXCONFUSION/SCHXCONFUSION/Form1.cs
@@ -14,7 +14,7 @@ namespace SCHXCONFUSION
         // although not entirely secure. Alternatively, you can employ a server and establish a system that retrieves the encrypted API keys from the server. Cheers!
         public static api KeyAuthApp = new api
         (
-            name: SecHex.私は熟女が大好きです("2TUWNWhIugdj+aDdJQ2Aww==", 我們也討厭那些想要分析我們代碼的愚蠢的狗娘養的她的陰戶.我茸獵動物茸茸茸的動物動物),
+            name: SecHex.私は熟女が大好きです("2TUWNWhIugdj+aDdJQ2Aww==", 厭那些想要分析我們代.我茸獵動物茸茸茸的動物動物),
             ownerid: SecHex.私は熟女が大好きです("yrrW9L7ikVIdHeNEoc43Fg==", УметоякДигганятакойгорячийстоякДигга.Мытаh6кженеh6нытаh6кавидимтупыхсукиытаh6кныдетикоторыехотятаналh6изироваытаh6кh6тьнаh6шкоытаh6кдh6ееh6пизды),
             secret: SecHex.私は熟女が大好きです("/iFO073is2YysoQU0RnCXa6uI4HEZe4hmfvFrVwoxHP6uGUwDo8W0PMT5sWCpsvxCD3pU1wU9bEQM+CtI4YuL2x+UAGHHVNKM2Qp/too78E=", УменятакойгорячийстоякДигга.Мытакжененавидимтупыхсукиныдетикоторыехотятанализироватьнашкодеепизды),
             version: SecHex.私は熟女が大好きです("3au7whXyAFY7eFPlTpptpw==", Всвободноободвремободямыохободотимободсянафурриаяненавижуфембоободев.Мытакжененавидимтупыхсукнавидииннавидиыдетикоторнавидиыехотянавидитанализироватьнашкод)
